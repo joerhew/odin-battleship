5451110c95179aac4f930e73850afbf97311898a
joerhew <joerhew@gmail.com>
2023-12-18 10:06:11 -0500

    Wrote additional tests for gameboard.js, and removed testing ship.js inside of gameboard.js by adding mock functions

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -10,12 +10,33 @@ describe('Gameboard class', () => {
   beforeEach(() => {
     gameboard = new Gameboard()
 
-    coordinates = [{ x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }]
-    ship = gameboard.placeShip(3, coordinates)
+    coordinates = [{x: 1, y: 2}, {x: 1, y: 3}, {x: 1, y: 4}]
+    ship = {
+      uuid: 'mocked-ship',
+      length: 3,
+      arrayOfCoordinates: coordinates,
+      setOrientation: jest.fn(),
+      rotate: jest.fn(),
+      move: jest.fn(),
+      receiveHit: jest.fn()
+    };
+    gameboard.ships.push(ship)
   })
   it('places a ship at specific coordinates by calling the ship constructor method', () => {
-
-    expect(ship.arrayOfCoordinates).toEqual(coordinates)
+    const newShip = gameboard.placeShip(1, [{x: 1, y: 1}])
+    expect(newShip.arrayOfCoordinates).toEqual([{x: 1, y: 1}])
+  })
+  it('rotates a ship', () => {
+    
+    gameboard.rotateShip(ship.uuid);
+    expect(ship.rotate).toHaveBeenCalled()
+  })
+  it('moves a ship', () => {
+    gameboard.moveShip(ship.uuid, {x: 3, y: 3})
+    const newPivotCellCoords = {x: 3, y: 3}
+    
+    
+    expect(ship.move).toHaveBeenCalledWith(newPivotCellCoords)
   })
 
   it(`receives an attack; if it's a hit, then records a hit for the ship and on the gameboard`, () => {
@@ -23,7 +44,7 @@ describe('Gameboard class', () => {
     
     gameboard.receiveAttack(attackCoordinates)
 
-    expect(ship.hits).toBe(1)
+    expect(ship.receiveHit).toHaveBeenCalled()
     expect(gameboard.attackedCoordinates.some(coord => coord.x === attackCoordinates.x && coord.y === attackCoordinates.y)).toBe(true)
     expect(gameboard.attackedCoordinates).toContainEqual({ x: 1, y: 3, hit: true })
   })
@@ -33,18 +54,18 @@ describe('Gameboard class', () => {
 
     gameboard.receiveAttack(attackCoordinates)
 
-    expect(ship.hits).toBe(0)
+    expect(ship.receiveHit).not.toHaveBeenCalled()
     expect(gameboard.attackedCoordinates.some(coord => coord.x === attackCoordinates.x && coord.y === attackCoordinates.y)).toBe(true)
     expect(gameboard.attackedCoordinates).toContainEqual({ x: 1, y: 5, hit: false })
   })
 
   it('reports whether or not all of the ships on the board have been sunk', () => {
-    const ArrayOfAttackCoordinates = [{ x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }]
-    ArrayOfAttackCoordinates.forEach(coords => {
+    const arrayOfAttackCoordinates = [{ x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }]
+    arrayOfAttackCoordinates.forEach(coords => {
       gameboard.receiveAttack(coords)
     })
 
-    expect(ship.hits).toBe(3)
+    expect(ship.receiveHit).toHaveBeenCalledTimes(3)
     expect(gameboard.areAllShipsHit()).toBe(true)
   })
 })
